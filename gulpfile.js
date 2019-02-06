cefd1e478d03d2a60ed1456c384fbb5407245607
wolf811 <wolf811@gmail.com>
2019-02-06 12:01:01 +0300

    ЛК - конференции, верстка готова

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,4 @@
 'use strict';
-// var gulp = require('gulp');
-// var bs = require('browser-sync').create();
-// var sass = require('gulp-sass');
-// var autoprefixer = require('gulp-autoprefixer');
-// var wait = require('gulp-wait')
-
-// gulp.task('browser-sync', ['sass'], function () {
-//     bs.init({
-//         server: {
-//             baseDir: "./"
-//         }
-//     });
-// });
-
-// gulp.task('sass', function(){
-//     return gulp.src('scss/*.scss')
-//         .pipe(wait(500))
-//         .pipe(sass({errLogToConsole: true}))
-//         .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
-//         .pipe(gulp.dest('css/'))
-//         .pipe(bs.reload({stream: true}));
-// });
-
-// gulp.task('watch', ['browser-sync'], function() {
-//     gulp.watch("scss/*.scss", ['sass']);
-//     gulp.watch("*.html").on('change', bs.reload);
-//     gulp.watch("includes/*.html").on('change', bs.reload);
-// })
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
